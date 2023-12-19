7b4814bfcbd397b442259ec8762a2d6c1ba65983
Mateo Salinas Clarke <mateosalinasclarke@proton.me>
2023-12-18 17:15:30 -0700

    add Login.gov [WIP]

--- app/src/pages/api/auth/[...nextauth].ts
+++ app/src/pages/api/auth/[...nextauth].ts
@@ -7,6 +7,20 @@ export const authOptions = {
       clientId: process.env.GITHUB_CLIENT_ID ?? "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
     }),
+    {
+      id: "logingov",
+      name: "Login.gov",
+      type: "oauth",
+      wellKnown:
+        "https://idp.int.identitysandbox.gov/.well-known/openid-configuration",
+      token: {
+        url: "https://idp.int.identitysandbox.gov/api/openid_connect/token",
+        params: { some: "param" },
+      },
+      clientId: process.env.LOGINGOV_CLIENT_ID ?? "",
+      acrValues: "http://idmanagement.gov/ns/assurance/ial/1",
+      nonce: "kbyv83qiqz41bgr32xj407d8by3qbyxl9ox8kohs9572u7p78aa",
+    },
     // ...add more providers here
   ],
 };
