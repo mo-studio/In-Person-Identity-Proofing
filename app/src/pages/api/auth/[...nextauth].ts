d7358c3d3be0590a59611423c98ac4c93d30e363
Mateo Salinas Clarke <mateosalinasclarke@proton.me>
2023-12-29 15:14:34 -0600

    use JWT instead of client_secret [WIP]

--- app/src/pages/api/auth/[...nextauth].ts
+++ app/src/pages/api/auth/[...nextauth].ts
@@ -23,15 +23,43 @@ export const authOptions = {
         "https://idp.int.identitysandbox.gov/.well-known/openid-configuration",
       token: {
         url: "https://idp.int.identitysandbox.gov/api/openid_connect/token",
-        params: { some: "param" },
+        auth: { method: "none" }, // Explicitly set the authentication method to 'none'
       },
       clientId: process.env.LOGINGOV_CLIENT_ID ?? "",
-      acrValues: "http://idmanagement.gov/ns/assurance/ial/1",
-      nonce: "12345",
+      clientSecret: null,
+      client: {
+        token_endpoint_auth_method: "private_key_jwt",
+      },
       nonce: generateNonce(),
+      authorization: {
+        params: {
+          scope: "openid email", // Adjust the scope as needed
+          acr_values: "http://idmanagement.gov/ns/assurance/ial/1", // Adjust the acr_values as per Login.gov requirements
+          nonce: generateNonce(),
+        },
+      },
     },
     // ...add more providers here
   ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user, session }) {
+      console.log("jwt callback", { token, user, session });
+      // Persist the OAuth access_token to the token right after signin
+      if (user) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      console.log("session callback", { session, token, user });
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
