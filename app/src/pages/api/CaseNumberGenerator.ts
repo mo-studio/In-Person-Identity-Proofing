f1f99e349615fb27d1ee6be415dd36c60b9a8f7b
“Anu <anu@themostudio.com>
2024-01-16 17:32:47 -0800

    lint

--- app/src/pages/api/CaseNumberGenerator.ts
+++ app/src/pages/api/CaseNumberGenerator.ts
@@ -1,8 +1,8 @@
 export function generateRandomCaseNumber(): string {
-  const digits: string = "0123456789";
-  let caseNumber: string = "";
+  const digits = "0123456789";
+  let caseNumber = "";
 
-  for (let i: number = 0; i < 8; i++) {
+  for (let i = 0; i < 8; i++) {
     const randomIndex: number = Math.floor(Math.random() * digits.length);
     caseNumber += digits.charAt(randomIndex);
   }
