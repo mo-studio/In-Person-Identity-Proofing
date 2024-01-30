fcbf7e5b8b8e3ccd36eacb8a219cc6be7d41d03f
“Anu <anu@themostudio.com>
2024-01-29 22:50:29 -0800

    case number generation tests

--- app/tests/pages/intake/2-select-site.test.tsx
+++ app/tests/pages/intake/2-select-site.test.tsx
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { initialIntakeData, IntakeContext } from "src/contexts/IntakeContext";
+import { generateRandomCaseNumber } from "src/pages/api/cases/NumberGenerator";
 import LocationSelectorScreen from "src/pages/intake/2-select-site/index";
 
 describe("LocationSelectorScreen", () => {
@@ -54,5 +55,24 @@ describe("LocationSelectorScreen", () => {
     fireEvent.click(searchButton);
     const continueButton = screen.getByText("Continue");
     expect(continueButton).toBeEnabled();
+  });
+
+  describe("generateRandomCaseNumber", () => {
+    it("should return a string of length 8", () => {
+      const caseNumber = generateRandomCaseNumber();
+      expect(caseNumber).toHaveLength(8);
+    });
+
+    it("should only contain digits", () => {
+      const caseNumber = generateRandomCaseNumber();
+      const regex = /^[0-9]+$/;
+      expect(caseNumber).toMatch(regex);
+    });
+
+    it("should return a different case number on each invocation", () => {
+      const caseNumber1 = generateRandomCaseNumber();
+      const caseNumber2 = generateRandomCaseNumber();
+      expect(caseNumber1).not.toEqual(caseNumber2);
+    });
   });
 });
