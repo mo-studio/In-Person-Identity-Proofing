645fd30b85fb906a70e0c0847c0204d7f5422371
“Anu <anu@themostudio.com>
2024-01-29 23:02:48 -0800

    db files

---
+++ app/prisma/migrations/20240130070140_init/migration.sql
@@ -0,0 +1,34 @@
+-- CreateEnum
+CREATE TYPE "CaseStatus" AS ENUM ('EXPIRED', 'SITE_CONFIRMED', 'INPERSON_PROOFED', 'UNCONFIRMED');
+
+-- CreateTable
+CREATE TABLE "Case" (
+    "case_number" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expired_at" TIMESTAMP(3),
+    "uuid" TEXT NOT NULL,
+    "status" "CaseStatus" NOT NULL,
+
+    CONSTRAINT "Case_pkey" PRIMARY KEY ("case_number")
+);
+
+-- CreateTable
+CREATE TABLE "UserCase" (
+    "id" SERIAL NOT NULL,
+    "case_number" INTEGER NOT NULL,
+    "user_id" SERIAL NOT NULL,
+    "expired_at" TIMESTAMP(3),
+    "proofing_agent_id" TEXT NOT NULL,
+
+    CONSTRAINT "UserCase_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProofingAgent" (
+    "user_id" SERIAL NOT NULL,
+    "logingov_id" INTEGER NOT NULL,
+    "role" TEXT NOT NULL,
+    "x_509" JSONB,
+
+    CONSTRAINT "ProofingAgent_pkey" PRIMARY KEY ("user_id")
+);
